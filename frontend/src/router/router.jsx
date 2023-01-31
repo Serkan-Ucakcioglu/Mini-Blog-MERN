72ae0af2bc274a8b46cfb60993b438939fa428df
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-31 16:23:11 +0300

    router add

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -6,12 +6,14 @@ import {
 } from "react-router-dom";
 import Tech from "../features/Category/Tech";
 import Car from "../features/Category/Car";
+import Finance from "../features/Category/Finance";
 
 export const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Tech />} />
       <Route path="/car" element={<Car />} />
+      <Route path="/finance" element={<Finance />} />
     </Route>
   )
 );
