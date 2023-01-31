9918cad0202d3baaddf647c6a3e4b7ea5f186091
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-31 14:41:53 +0300

    add blog controller and route add

--- backend/controlelr/blogController.js
+++ backend/controlelr/blogController.js
@@ -10,4 +10,17 @@ const getAllBlog = async (req, res) => {
   }
 };
 
-module.exports = getAllBlog;
+const addBlog = async (req, res) => {
+  try {
+    const { title, url, description } = req.body;
+    const checkList = await Blog.findOne({ title });
+    if (checkList) {
+      return res.status(404).json("already on this blog");
+    }
+    const newBlog = await Blog.create({ title, url, description });
+  } catch (error) {
+    res.status(404).json(error?.message);
+  }
+};
+
+module.exports = { getAllBlog, addBlog };
--- backend/router/blogRoute.js
+++ backend/router/blogRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const getAllBlog = require("../controlelr/blogController");
+const { getAllBlog, addBlog } = require("../controlelr/blogController");
 
 const router = express.Router();
 
 router.get("/blogList", getAllBlog);
+router.post("/add-blog", addBlog);
 
 module.exports = router;
