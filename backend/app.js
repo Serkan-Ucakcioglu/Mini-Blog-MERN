dcdaab73279999d39d25caecb3a4ef7800e66043
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-31 14:33:58 +0300

    controller create router create and router use app

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,11 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 const dbConnect = require("./config/dbConnect");
+const blogRoute = require("./router/blogRoute");
 
 dbConnect();
 
-app.get("/", (req, res) => {
-  res.send("hello from simple server :)");
-});
+app.use("/", blogRoute);
 
 app.listen(port, () =>
   console.log("> Server is up and running on port : " + port)
