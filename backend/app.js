26adf9b6ecf79101580ec152b0012751db1223a9
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-31 14:44:58 +0300

    cors add

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 4000;
 const dbConnect = require("./config/dbConnect");
 const blogRoute = require("./router/blogRoute");
 
 dbConnect();
-
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use("/", blogRoute);
+app.use(express.json());
 
 app.listen(port, () =>
   console.log("> Server is up and running on port : " + port)
