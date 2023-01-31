c723964c3621e54a8954342a8775b053b3008960
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-31 16:26:36 +0300

    active class add

--- frontend/src/components/header/HeadTitle.jsx
+++ frontend/src/components/header/HeadTitle.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function HeadTitle() {
   return (
     <div>
       <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-        <Link to="/" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+        <NavLink to="/" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
           Technology
-        </Link>
-        <Link to="/car" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+        </NavLink>
+        <NavLink to="/car" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
           Car
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="/finance"
           className="hover:bg-gray-400 rounded py-2 px-4 mx-2"
         >
           Finance
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
