2cc271afe8dca939aab27b31374fdd479a1553fc
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-31 15:23:03 +0300

    appjs outlet add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Outlet />
     </div>
   );
 }
