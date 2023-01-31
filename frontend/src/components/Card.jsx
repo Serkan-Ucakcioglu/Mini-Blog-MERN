00dd1d8c087f024d956c3d73c40520540d9c9893
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-31 16:34:37 +0300

    updated

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ blog }) {
   return (
     <article className="flex flex-col shadow my-4">
       <a href="#" className="hover:opacity-75">
-        <img src={blog?.url} loading="lazy" />
+        <img src={blog?.url} loading="lazy" className="h-[400px] w-full" />
       </a>
       <div className="bg-white flex flex-col justify-start p-6">
         <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">
