701c604a91f83c91448aafc6a5b9a13750fa8f2c
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-31 16:22:48 +0300

    link add

--- frontend/src/components/header/HeadTitle.jsx
+++ frontend/src/components/header/HeadTitle.jsx
@@ -5,15 +5,18 @@ function HeadTitle() {
   return (
     <div>
       <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-        <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+        <Link to="/" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
           Technology
-        </a>
+        </Link>
         <Link to="/car" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
           Car
         </Link>
-        <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+        <Link
+          to="/finance"
+          className="hover:bg-gray-400 rounded py-2 px-4 mx-2"
+        >
           Finance
-        </a>
+        </Link>
       </div>
     </div>
   );
