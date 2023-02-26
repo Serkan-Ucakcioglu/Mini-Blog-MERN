5b09bce5035d837d6a0a6afebdcfa154792b7320
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-26 13:40:33 +0300

    convert

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 import store from "./store/index";
 import { router } from "./router/router";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <RouterProvider router={router}>
       <App />
